ab410837cb1b269bd7e57617a0852242ba51fb01
dylan <solon.dylan@laposte.net>
2024-04-04 11:17:24 +0200

    feature: add MessageModel

--- index.php
+++ index.php
@@ -9,10 +9,12 @@ use App\Controllers\User;
 use App\Controllers\Reponses;
 use App\Controllers\Bot;
 use App\Controllers\Bots;
+use App\Controllers\Messages;
 
 new Router([
     'user/:id' => User::class,
     'reponses/:id' => Reponses::class,
     'bot/:id' => Bot::class,
-    'bots' => Bots::class
+    'bots' => Bots::class,
+    'messages' => Messages::class
 ]);
