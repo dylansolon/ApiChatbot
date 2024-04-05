87a8ae5b4607a028a5735d7c90e4f191efdcd8c0
Mattoucoding <mattcoding45@gmail.com>
2024-04-05 14:18:00 +0200

    Reponses.php > stock les reponses via id

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <?php
 
-// header("Access-Control-Allow-Origin: *");
-
 require 'vendor/autoload.php';
 
 use App\Router;
 use App\Controllers\User;
-use App\Controllers\Reponses;
 use App\Controllers\Bot;
 use App\Controllers\Bots;
 use App\Controllers\Messages;
+use App\Controllers\Reponses;
 
 new Router([
     'user/:id' => User::class,
-    'reponses/:id' => Reponses::class,
     'bot/:id' => Bot::class,
     'bots' => Bots::class,
-    'messages' => Messages::class
+    'messages' => Messages::class,
+    'reponses/:id' => Reponses::class
+
 ]);
